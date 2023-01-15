2f54b7a0ca3f55130b8db436c2e2bcbf68b92063
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-01-15 21:27:22 +0100

    omg static files

--- catShoesStore/settings.py
+++ catShoesStore/settings.py
@@ -126,17 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 ALLOWED_HOSTS = ['*']
 
 import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 if DEBUG:
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'staticfiles')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
