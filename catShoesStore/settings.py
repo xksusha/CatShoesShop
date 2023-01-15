71148f508d31d15fb4152c0c035c387144cfa5c2
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-01-15 21:14:19 +0100

    any allowed host

--- catShoesStore/settings.py
+++ catShoesStore/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+ALLOWED_HOSTS = ['*']
+
 import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
