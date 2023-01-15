68e757d33f71105f0887e9252ed50ea579b107e8
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-01-15 21:29:50 +0100

    omg static files

--- catShoesStore/settings.py
+++ catShoesStore/settings.py
@@ -131,12 +131,8 @@ ALLOWED_HOSTS = ['*']
 
 import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'staticfiles')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
