8492954c367464585c005357f2a80d43a1742e4a
Julien Gilli <jgilli@fastmail.fm>
2023-03-07 16:52:41 +0100

    test

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
+# <DT> [chore] Should save to db and not compute
 router = routers.SimpleRouter()
 router.register(r'', views.OrderViews)
 urlpatterns = router.urls
+# </DT>
