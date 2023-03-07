8c40609decc1aa9a084208e1f8025d92720a87df
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-03-07 16:26:36 +0100

    back to real file lists

--- tech/index.js
+++ tech/index.js
@@ -21,23 +21,25 @@ const handleFilesList = (filesList = []) => {
 }
 
 async function main() {
-    // added_files_path = process.argv[2]
-    // console.log(`added_files_path: ${added_files_path}`)
+    added_files_path = process.argv[2]
+    console.log(`added_files_path: ${added_files_path}`)
 
-    // removed_files_path = process.argv[3]
-    // console.log(`removed_files_path: ${removed_files_path}`)
+    removed_files_path = process.argv[3]
+    console.log(`removed_files_path: ${removed_files_path}`)
 
-    // modified_files_path = process.argv[4]
-    // console.log(`modified_files_path: ${modified_files_path}`)
+    modified_files_path = process.argv[4]
+    console.log(`modified_files_path: ${modified_files_path}`)
 
-    // added_files_list = JSON.parse(await readFile(added_files_path))
-    // removed_files_list = JSON.parse(await readFile(removed_files_path))
-    // modified_files_list = JSON.parse(await readFile(modified_files_path))
+    added_files_list = JSON.parse(await readFile(added_files_path))
+    removed_files_list = JSON.parse(await readFile(removed_files_path))
+    modified_files_list = JSON.parse(await readFile(modified_files_path))
 
-    // console.log(`added_files_list: ${added_files_list}`)
-    // console.log(`removed_files_list: ${removed_files_list}`)
-    // console.log(`modified_files_list: ${modified_files_list}`)
-    handleFilesList(['../orders/urls.py'])
+    console.log(`added_files_list: ${added_files_list}`)
+    console.log(`removed_files_list: ${removed_files_list}`)
+    console.log(`modified_files_list: ${modified_files_list}`)
+    handleFilesList(modified_files_list)
+    handleFilesList(removed_files_list)
+    handleFilesList(added_files_list)
 }
 
 main()
