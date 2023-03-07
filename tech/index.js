c92a863c4835eff68733808fd73fc6adb26f75d3
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-03-07 14:54:07 +0100

    first draft notion api

---
+++ tech/index.js
@@ -0,0 +1,6 @@
+const { NotionAPI } = require('./notionApi.js')
+
+console.log(NotionAPI)
+// api = new NotionAPI()
+
+// api.then(results => console.log(results))
---
+++ tech/notionApi.js
@@ -0,0 +1,21 @@
+'use strict';
+const { Client } = require("@notionhq/client")
+
+module.exports = class NotionAPI {
+  constructor(apiToken='secret_AFCZD8PHefoXQT8Gp1UlvRtADXOag5NnIKWQwVE2oZf') {
+    this.notion = new Client({
+      auth: apiToken,
+    })
+  }
+  findItemsByFilename(filename, databaseID='ee64fcde89b541b5b410ad9974847bbb') {
+    return notion.databases.query({
+      database_id: databaseID,
+      filter: {
+        property: "filename",
+        rich_text: {
+          contains: filename,
+        },
+      },
+    })
+  }
+}
