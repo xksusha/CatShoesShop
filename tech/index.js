20b0db8faa890fbc34ddfa03f0e2b1c434d07263
Ksenia Kanishcheva <kseniakanishcheva@Ksenias-MacBook-Air.local>
2023-03-07 16:22:48 +0100

    alighning

--- tech/index.js
+++ tech/index.js
@@ -14,30 +14,30 @@ const handleFilesList = (filesList = []) => {
     api.findItemsByFilename(filename).then(data => data.results.forEach(r => {
       return api.deletePage(r.id)
     }))
-    get_debt_comments(filename).then(comments => comments.forEach(comment => {
+    get_debt_comment_from_file(filename).then(comments => comments.forEach(comment => {
       api.insertItem(comment)
     }))
   })
 }
 
 async function main() {
-    added_files_path = process.argv[2]
-    console.log(`added_files_path: ${added_files_path}`)
+    // added_files_path = process.argv[2]
+    // console.log(`added_files_path: ${added_files_path}`)
 
-    removed_files_path = process.argv[3]
-    console.log(`removed_files_path: ${removed_files_path}`)
+    // removed_files_path = process.argv[3]
+    // console.log(`removed_files_path: ${removed_files_path}`)
 
-    modified_files_path = process.argv[4]
-    console.log(`modified_files_path: ${modified_files_path}`)
+    // modified_files_path = process.argv[4]
+    // console.log(`modified_files_path: ${modified_files_path}`)
 
-    added_files_list = JSON.parse(await readFile(added_files_path))
-    removed_files_list = JSON.parse(await readFile(removed_files_path))
-    modified_files_list = JSON.parse(await readFile(modified_files_path))
+    // added_files_list = JSON.parse(await readFile(added_files_path))
+    // removed_files_list = JSON.parse(await readFile(removed_files_path))
+    // modified_files_list = JSON.parse(await readFile(modified_files_path))
 
-    console.log(`added_files_list: ${added_files_list}`)
-    console.log(`removed_files_list: ${removed_files_list}`)
-    console.log(`modified_files_list: ${modified_files_list}`)
-    handleFilesList(modified_files_list)
+    // console.log(`added_files_list: ${added_files_list}`)
+    // console.log(`removed_files_list: ${removed_files_list}`)
+    // console.log(`modified_files_list: ${modified_files_list}`)
+    handleFilesList(['../orders/urls.py'])
 }
 
 main()
